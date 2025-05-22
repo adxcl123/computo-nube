749b1b74b32066bf4408be2482fd3e66479984ba
Aby <150267931+adxcl123@users.noreply.github.com>
2025-05-22 16:30:36 -0600

    conexion6

--- index.php
+++ index.php
@@ -141,7 +141,7 @@
                     $serverName = "tcp:bdserversql.database.windows.net,1433";
                     $database = "bdsql01";
                     $username = "adminsql";
-                    $password = "{your_password_here}";
+                    $password = "Servid0r1";
                     
                     $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
                     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
